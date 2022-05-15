ea32220b9d79209fae28eb845c55cdf6eac0c393
Dima Krasner <dima@dimakrasner.com>
2022-05-16 00:56:36 +0800

    papaw: update copyright year

--- papaw.c
+++ papaw.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of papaw.
  *
- * Copyright (c) 2019, 2020, 2021 Dima Krasner
+ * Copyright (c) 2019, 2020, 2021, 2022 Dima Krasner
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
